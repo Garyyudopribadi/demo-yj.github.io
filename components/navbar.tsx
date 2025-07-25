a6fe082b087081707616552228cebd0e2427c467
Garyudo <garyyudo@hotmail.com>
2025-07-25 14:28:58 +0700

    Demo still login

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,7 +5,6 @@ import Link from "next/link"
 import { usePathname } from "next/navigation"
 import { Menu, User } from "lucide-react"
 import { Button } from "@/components/ui/button"
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 import { Sheet, SheetContent, SheetTrigger, SheetTitle } from "@/components/ui/sheet"
 import GoogleTranslate from "./google-translate"
 
